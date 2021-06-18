a68e9b7cf50f7fe68968900d200aa2cf42cf1192
Priyaraj7 <suppichala@gmail.com>
2021-06-18 14:22:49 -0700

    node learning project

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+
+const morgan = require("morgan");
+
+const mongoose = require("mongoose");
+
+const blogRoutes = require("./routes/blogRoutes");
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://Supriya:HnU3kDFscIKEjnGb@cluster0.azllq.mongodb.net/node-tote?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// mongoose & mongo tests
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("5ea99b49b8531f40c0fde689")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+// blog routes
+app.use("/blogs", blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
